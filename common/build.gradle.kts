84636e083247c0f8f9c1f977ccc80e885659e2d6
Geoff Powell <gpowell@squareup.com>
2024-02-19 13:33:29 -0500

    Initial Multiplatform App

---
+++ common/build.gradle.kts
@@ -0,0 +1,133 @@
+plugins {
+  kotlin("multiplatform")
+  id("com.android.library")
+  alias(libs.plugins.compose)
+  alias(libs.plugins.sqldelight)
+  alias(libs.plugins.spotless)
+}
+
+group = "com.greenmiststudios.buddybuild"
+version = "${project.properties["buddybuild.version_name"]?.toString()}-SNAPSHOT"
+
+@OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+kotlin {
+
+  androidTarget {
+    compilations.all {
+      kotlinOptions.jvmTarget = "11"
+    }
+  }
+  jvm {
+    compilations.all {
+      kotlinOptions.jvmTarget = "11"
+    }
+  }
+  js(IR) {
+    browser()
+  }
+
+  iosX64()
+  iosArm64()
+  iosSimulatorArm64()
+
+  sourceSets {
+    val commonMain by getting {
+      dependencies {
+        api(compose.runtime)
+        api(compose.ui)
+        api(compose.foundation)
+        api(compose.materialIconsExtended)
+        api(compose.material)
+        api(compose.material3)
+        api(libs.voyager.navigator)
+        api(libs.voyager.tab.navigator)
+        api(libs.voyager.bottom.sheet.navigator)
+        api(libs.koin.core)
+        api(libs.koin.compose)
+        api(libs.uuid)
+        api(libs.sqldelight.coroutines.extensions)
+
+        implementation(libs.molecule.runtime)
+      }
+    }
+
+    val commonTest by getting {
+      dependencies {
+        implementation(kotlin("test"))
+      }
+    }
+
+    val androidMain by getting {
+      dependencies {
+        api(libs.androidx.appcompat)
+        api(libs.androidx.core)
+        api(libs.kotlinx.coroutines.android)
+        api(libs.koin.android)
+        api(libs.koin.androidx.compose)
+
+        implementation(libs.sqldelight.driver.android)
+      }
+    }
+
+    nativeMain.dependencies {
+      implementation(libs.sqldelight.driver.native)
+    }
+
+    val jvmMain by getting {
+      dependencies {
+        api(compose.preview)
+        implementation(libs.sqldelight.driver.jvm)
+      }
+    }
+
+    val jsMain by getting {
+      dependencies {
+        api(compose.html.core)
+        implementation(libs.sqldelight.driver.web)
+        implementation(npm("@cashapp/sqldelight-sqljs-worker", libs.versions.sqldelight.get()))
+        implementation(npm("sql.js", "1.8.0"))
+        implementation(devNpm("copy-webpack-plugin", "9.1.0"))
+      }
+    }
+
+    iosMain.dependencies {
+      // Fixes sqldelight 2.0.1 issue with ios compilation
+      // https://github.com/cashapp/sqldelight/issues/4888
+      implementation("co.touchlab:stately-common:2.0.6")
+    }
+
+    // val wasmJsMain by getting {
+    //     dependencies {
+    //         implementation(libs.ktor.core.wasm)
+    //     }
+    // }
+  }
+
+  explicitApi()
+}
+
+android {
+  namespace = "com.greenmiststudios.buddybuild.common"
+  compileSdk = libs.versions.android.compileSdk.get().toInt()
+  sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+  defaultConfig {
+    minSdk = libs.versions.android.minSdk.get().toInt()
+    targetSdk = libs.versions.android.targetSdk.get().toInt()
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+}
+dependencies {
+  debugImplementation(libs.compose.ui.tooling.preview.android)
+}
+
+sqldelight {
+  databases {
+    create("Database") {
+      packageName.set("com.greenmiststudios.buddybuild")
+    }
+    linkSqlite = true
+  }
+}
---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,134 @@
+import org.jetbrains.compose.ExperimentalComposeLibrary
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+
+plugins {
+  alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.compose)
+  alias(libs.plugins.paparazzi)
+  alias(libs.plugins.spotless)
+}
+
+val projectVersion = project.ext.get("version")?.toString()
+
+kotlin {
+  js(IR) {
+    browser {
+      useCommonJs()
+      binaries.executable()
+    }
+  }
+
+  androidTarget {
+    compilations.all {
+      kotlinOptions {
+        jvmTarget = "11"
+      }
+    }
+  }
+
+  jvm("desktop") {
+    compilations.all {
+      kotlinOptions.jvmTarget = "17"
+    }
+  }
+
+  listOf(
+    iosX64(),
+    iosArm64(),
+    iosSimulatorArm64()
+  ).forEach { iosTarget ->
+    iosTarget.binaries.framework {
+      baseName = "ComposeApp"
+      isStatic = true
+    }
+  }
+
+  sourceSets {
+    val desktopMain by getting
+
+    androidMain.dependencies {
+      implementation(libs.androidx.activity.compose)
+    }
+    commonMain.dependencies {
+      implementation(libs.koin.compose)
+
+      @OptIn(ExperimentalComposeLibrary::class)
+      implementation(compose.components.resources)
+      implementation(projects.common)
+    }
+    desktopMain.dependencies {
+      implementation(compose.desktop.currentOs)
+    }
+    iosMain.dependencies {
+    }
+    jsMain.dependencies {
+      api(compose.html.core)
+      implementation(libs.sqldelight.driver.web)
+      implementation(npm("@cashapp/sqldelight-sqljs-worker", libs.versions.sqldelight.get()))
+      implementation(npm("sql.js", "1.8.0"))
+      implementation(devNpm("copy-webpack-plugin", "9.1.0"))
+    }
+  }
+}
+
+android {
+  namespace = "com.greenmiststudios.buddybuild"
+  compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+  sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+  sourceSets["main"].res.srcDirs("src/androidMain/res")
+  sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
+  defaultConfig {
+    applicationId = "com.greenmiststudios.buddybuild"
+    minSdk = libs.versions.android.minSdk.get().toInt()
+    targetSdk = libs.versions.android.targetSdk.get().toInt()
+    versionCode = project.ext.get("versionBuild")?.toString()?.toInt()
+    versionName = projectVersion
+  }
+  packaging {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+  }
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = true
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+  dependencies {
+    debugImplementation(libs.compose.ui.tooling)
+  }
+}
+dependencies {
+  implementation(projects.common)
+}
+
+compose.desktop {
+  application {
+    mainClass = "MainKt"
+
+    nativeDistributions {
+      targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+      packageName = "com.greenmiststudios.buddybuild"
+      packageVersion = projectVersion
+    }
+  }
+}
+
+compose.experimental {
+  web.application {}
+}
+
+afterEvaluate {
+  rootProject.extensions.configure<NodeJsRootExtension> {
+    versions.webpackDevServer.version = "4.0.0"
+    versions.webpackCli.version = "4.10.0"
+  }
+}
